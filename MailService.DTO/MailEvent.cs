77fc7e90f67fa3c63a47bc0e587f28d7102e7940
ErikBortoluzzi <erik.bortoluzzi@stud.tecnicosuperiorekennedy.it>
2021-12-16 14:42:13 +0100

    mailevent creation

--- MailService.DTO/MailEvent.cs
+++ MailService.DTO/MailEvent.cs
@@ -4,6 +4,10 @@ namespace MailService.DTO
 {
     public class MailEvent
     {
-
+        public String To { get; set; }
+        public String Cc { get; set; }
+        public String Ccn { get; set; }
+        public String Subject { get; set; }
+        public String Body { get; set; }
     }
 }
